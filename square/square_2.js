71cba9ba42baa3fd383cc3fe570224bfb0c9ef65
Nikola Stanchev <nikolastan4ev@gmail.com>
2022-11-10 20:15:44 +0200

    update

--- square/square_2.js
+++ square/square_2.js
@@ -4,10 +4,10 @@ function square(n) {
     for (let j = 1; j <= n - 2; j++) {
         if (j === Math.ceil((n - 2) / 2)) {
             let row3 = "x" + " ".repeat((n - 3) / 2) + "x" + " ".repeat((n - 3) / 2) + "x";
-            console.log(row3)
+            document.getElementById("square").innerHTML = row3;
         } else {
             let row2 = "x" + " ".repeat(n - 2) + "x";
-            console.log(row2)
+            document.getElementById("square").innerHTML = row2;
         }
     }
     console.log(row)
