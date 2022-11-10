eb4737a772adc082a5ce155d927794211dab635d
nikist98 <87224924+nikist98@users.noreply.github.com>
2022-11-10 20:26:51 +0200

    Update square_2.js
    updated

--- square/square_2.js
+++ square/square_2.js
@@ -1,15 +1,17 @@
-function square(n) {
+let n = Number(document.getElementById("input"))
+ 
+function square() {
     let row = "x".repeat(n);
-    console.log(row)
+    document.getElementById.innerHTML("square") = row
     for (let j = 1; j <= n - 2; j++) {
         if (j === Math.ceil((n - 2) / 2)) {
             let row3 = "x" + " ".repeat((n - 3) / 2) + "x" + " ".repeat((n - 3) / 2) + "x";
-            document.getElementById("square").innerHTML = row3;
+            document.getElementById.innerHTML("square") = row3
         } else {
             let row2 = "x" + " ".repeat(n - 2) + "x";
-            document.getElementById("square").innerHTML = row2;
+            document.getElementById.innerHTML("square") = row2
         }
     }
-    console.log(row)
+    document.getElementById.innerHTML("square") = row
 }
-square(9)
+square()
